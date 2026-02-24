25a13cc9396ae2d82e5c4830d3a74381e014cd8b
Chance Stahura <chance.stahura68@gmail.com>
2026-02-23 20:21:49 -0800

    feat: update Go HTTP server to return JSON object with name and unix timestamp

--- cmd/app.go
+++ cmd/app.go
@@ -1,29 +1,36 @@
 package main
 
 import (
-	//"encoding/json" 	//uses struct(message) to encode(marshal) and decode(unmarshal)
-	//"net/http"		//functionality for both HTTP clients/servers
-	//"time"			//getting the current system time
+	"encoding/json" 	//uses struct(message) to encode(marshal) and decode(unmarshal)
+	"time"			//getting the current system time
 	"log"
 	"github.com/gofiber/fiber/v3"
 )
 
+type message struct {
+	Message string `json:"message"`
+	Timestamp int64 `json:"timestamp"`
+}
 
-//type message struct {
-//	message string `json:"message"`
-//	timestamp time.Time `json:"timestamp"`
-//}
+func serveMessage(c fiber.Ctx) error {
+	m := message{
+		Message: "My name is Chance", 
+		Timestamp: time.Now().Unix(),
+	}
+
+	c.Set("Content-Type", "application/json")
+	data, _ := json.MarshalIndent(m, "", "  ")
+	return c.SendString(string(data))
+}
 
 
 func main() {
     app := fiber.New()
 
 	//GET route for the endpoint
-    app.Get("/", func(c fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", serveMessage)
 
 	//start server on port 3000
+	//listen for incoming HTTP requests
     log.Fatal(app.Listen(":3000"))
-
 }
